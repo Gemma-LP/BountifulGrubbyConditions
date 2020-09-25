d1e50f60ab2d79bbea14a68aac506f007748c4bd
Gemma P <g_perry@ymail.com>
2020-09-25 23:13:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+joke = 
+input("Hi there! Select a number between 1 and 5 to hear a joke."
+if 1 == Did you hear about the mathematician who was afraid of negative numbers? He would dtop at nothing to avoid them
