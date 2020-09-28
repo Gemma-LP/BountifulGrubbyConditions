8e6f7ad55526ad6e99ffa6e2a55c123c0e7c58a9
Gemma Perry <g_perry@ymail.com>
2020-09-28 18:07:31 +0000

    calculator

--- main.py
+++ main.py
@@ -1,3 +1,56 @@
-joke = 
-input("Hi there! Select a number between 1 and 5 to hear a joke."
-if 1 == Did you hear about the mathematician who was afraid of negative numbers? He would dtop at nothing to avoid them
+bikecost = 2000
+print("If a Motorbike costs £",bikecost,"and it's value decreses by 10% each year, the value will fall as follows:\n")
+
+while (bikecost > 1000):
+  print("£",bikecost)
+  deduction = bikecost / 10
+  bikecost = bikecost - deduction
+else:
+  print("\nAfter this point, the value of the motorbike falls below £1000.")
+
+
+print("\n\nHi there!")
+while True:
+  jokechoice = int(input("Select a number for a joke from 1, 2 or 3. "))
+  if (jokechoice ==1):
+    print("Did you hear about the mathematician who was afraid od negative numbers...he would stop at nothing to avoid them.")
+  elif (jokechoice ==2):
+    print ("What does a nosey pepper do? Gets jalapeno business!")
+  elif (jokechoice ==3):
+    print("What do you call a parade of rabbits hopping backwards? A redeecding hare-line.")
+  elif (jokechoice ==0):
+    break
+else:
+  print("That's not a valid selection. Try again.")
+
+def add(x, y):
+  return x + y
+def subtract(x, y):
+  return x - y
+def multiply (x,y):
+  return x * y
+def divide(x, y):
+  return x / y 
+print("Select operation.")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+while True:
+  choice = input("Enter your selection (1/2/3/4): ")
+  if choice in ('1', '2', '3', '4'):
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    if choice =='1': 
+      print(num1, "+", num2, "=", add(num1, num2))
+    elif choice =='2': 
+      print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice =='3':
+      print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice =='4':
+      print(num1, "/", num2, "=", divide(num1, num2))
+    break
+  else:
+    print("Invalid input")
+
+
